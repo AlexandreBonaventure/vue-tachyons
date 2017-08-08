38a9d611b3ef840ca43d3c98ddca6fb4af3aea8b
Alexandre Bonaventure <ageissmann@lichencommunications.com>
2017-08-08 19:14:53 -0400

    🐛 fix regexp + snakecase

--- src/v1.js
+++ src/v1.js
@@ -1,6 +1,6 @@
 
 import cxs from 'cxs/atomic'
-import { snakeCase, compact } from 'lodash-es'
+import { compact } from 'lodash-es'
 import t from 'tachyons-js'
 import 'tachyons-js/variables.css'
 import { addClass } from './utils/index.js'
@@ -8,9 +8,9 @@ import { addClass } from './utils/index.js'
 const directive = {
   update (newValue, oldValue) {
     const classes = compact(newValue.split('.')).map(c => {
-      const pattern = /^([a-z]+(?:-{1,2}[a-z]+)*?)([0-9]+)?(-[a-z]{1,2})?$$/gi
-      return c.replace(pattern, (match, p1, unit, mqModifier, offset, string) =>
-        `${snakeCase(p1)}${unit || ''}${mqModifier ? '_' + mqModifier : ''}`)
+      const pattern = /^([a-z]+(?:-{1,2}[a-z]+)*?-?)([0-9]+)?(-[a-z]{1,2})?$/gi
+      return c.replace(pattern, (match, core, unit = '', mqModifier = '') =>
+        `${core.replace(/-/g, '_')}${unit}${mqModifier && '_' + mqModifier}`)
     })
     const className = cxs({
       ...classes.reduce((sum, tClass) => {
